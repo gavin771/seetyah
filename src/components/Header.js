cf1f1dfdef33f66491b05e180ed375fdbfab5c97
Gavin Samuels <gavin.samuels10@gmail.com>
2019-09-01 11:01:14 -0500

    modified structure of page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import {
+  Container,
+  Segment,
+  Button,
+  Form,
+  Input,
+  Header as SemanticHeader,
+} from 'semantic-ui-react'
 
-const Header = (props) => (
-    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
-        <div className="logo">
-            <span className="icon fa-diamond"></span>
+const Header = props => {
+  const [workspace, setWorkspace] = useState('')
+
+  return (
+    <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+      <Container textAlign="center">
+        <SemanticHeader as="h1" className="main-heading">
+          Collaborate Fast | No Authentication | Easy Sharing
+        </SemanticHeader>
+        <p style={{ margin: '0px 15em 30px' }}>
+          Create a workspace and access it anywhere at anytime. Upload
+          screenshots, paste long links, and easily reference them from one
+          place. Try it!
+        </p>
+        <div className="segment-container">
+          <SemanticHeader as="h3" attached="top" className="segment-heading">
+            Create a workspace or join an existing one
+          </SemanticHeader>
+          <Segment textAlign="center" raised attached padded compact>
+            <Form>
+              <Form.Field
+                control={Input}
+                placeholder="Team, Project or Name"
+                value={workspace}
+                onChange={e => setWorkspace(e.target.value)}
+              />
+            </Form>
+          </Segment>
+          <Button.Group attached="bottom">
+            <Button disabled={workspace ? false : true} color="green">
+              Create
+            </Button>
+            <Button disabled={workspace ? false : true} color="blue">
+              Join
+            </Button>
+          </Button.Group>
         </div>
-        <div className="content">
-            <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
-            </div>
-        </div>
-        <nav>
-            <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
-            </ul>
-        </nav>
+      </Container>
     </header>
-)
+  )
+}
 
 Header.propTypes = {
-    onOpenArticle: PropTypes.func,
-    timeout: PropTypes.bool
+  openWorkspace: PropTypes.func,
+  timeout: PropTypes.bool,
 }
 
 export default Header
