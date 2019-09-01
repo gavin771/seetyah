0ac176824c2b4b42a6445c7791b4f665bf1f7394
Gavin Samuels <gavin.samuels10@gmail.com>
2019-09-01 11:00:07 -0500

    imported semantic & updated gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    title: 'Seetyah | Easy to use workspace for images, links and more',
+    author: '@gavin771',
+    description: 'An easy to use workspace for images, links and more',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -12,13 +12,13 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#34b3a0',
+        theme_color: '#34b3a0',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
-    'gatsby-plugin-offline'
+    'gatsby-plugin-offline',
   ],
 }
